a98e7979866fc5e77095c8be07a9af05e8cca678
Steve Lefevre <lefevre.10@osu.edu>
2021-04-05 15:03:20 -0400

    Added answers to question seeder

--- database/seeds/QuestionSeeder.php
+++ database/seeds/QuestionSeeder.php
@@ -16,26 +16,31 @@ class QuestionSeeder extends Seeder
         \DB::table('questions')->insert([
             [
                 'wording' => "What is the average air speed velocity of a laden swallow?",
+                'answer' => '12',
                 'created_at' => Carbon::now(),
                 'sort' => ++$i,
             ],
             [
                 'wording' => "What do you get when you multiply six by nine?",
+                'answer' => '42',
                 'created_at' => Carbon::now(),
                 'sort' => ++$i,
             ],
             [
                 'wording' => "What is your father's middle name?",
+                'answer' => 'George',
                 'created_at' => Carbon::now(),
                 'sort' => ++$i,
             ],
             [
                 'wording' => "How many licks does it take to get to the tootsie roll center of a tootsie pop?",
+                'answer' => '300',
                 'created_at' => Carbon::now(),
                 'sort' => ++$i,
             ],
             [
                 'wording' => "What would you do for a Klondike bar?",
+                'answer' => 'Anything',
                 'created_at' => Carbon::now(),
                 'sort' => ++$i,
             ],
