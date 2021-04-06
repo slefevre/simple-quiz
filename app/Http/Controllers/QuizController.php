378bfd6bcb66d7905975eb5fc403ff300c2c0eff
Steve Lefevre <lefevre.10@osu.edu>
2021-04-06 14:10:41 -0400

    Added comments to quiz controller

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -7,12 +7,18 @@ use App\Question;
 
 class QuizController extends Controller
 {
+    /**
+     * Present the quiz questions to the user
+     */
     public static function quiz() {
         return view('quiz', [
             'questions' => Question::orderBy('sort')->get(),
         ]); 
     }
     
+    /**
+     * Show the user their score
+     */
     public static function respond(Request $request) {
         return view('answers');
     }
