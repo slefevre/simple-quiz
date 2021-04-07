6e1f50cc9b154032e06a77ffcec6edb8d8d2e847
Steve Lefevre <lefevre.10@osu.edu>
2021-04-07 01:00:25 -0400

    Fleshed out Quiz model

--- app/Quiz.php
+++ app/Quiz.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Quiz extends Model
 {
     //
+    protected $table = 'quizzes';
+    
+    public static function answers() {
+        return $this->hasMany('App\Answer');
+    }
 }
