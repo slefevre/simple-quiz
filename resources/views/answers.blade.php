e24b7f10cbe17deb8bf8924f3e4606d4cfd60283
Steve Lefevre <lefevre.10@osu.edu>
2021-04-07 00:43:35 -0400

    Displaying answers

--- resources/views/answers.blade.php
+++ resources/views/answers.blade.php
@@ -1,1 +1,37 @@
-Answers go here
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Quiz</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+
+    <h1>Score</h1>
+
+    @if($errors->any())
+        @foreach ($errors->all() as $error)
+            {{ $error }}<br/>
+        @endforeach
+    @endif
+
+    @isset($answers)
+    <ul>
+        @foreach( $answers as $answer )
+		<li>{{ $answer }}<br/>
+        @endforeach
+    </ul>
+    @endisset
+
+    </body>
+</html>
